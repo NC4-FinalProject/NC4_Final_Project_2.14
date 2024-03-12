10d8bb1820cae5f77c67b2b9677682ca4b302221
Robbie-Kim <robbie6673@naver.com>
2024-03-12 21:43:13 +0900

    feat : header, footer css 수정

--- src/components/ui/layout/Footer.js
+++ src/components/ui/layout/Footer.js
@@ -4,13 +4,24 @@ import '../../../scss/Footer.scss';
 const Footer = () => {
   return (
     <div className="Footer">
-        <div className="container">
-            <img src='images/location.svg' alt='위치 바로가기'/>
+      <div className="container">
+        <div className='location'>
+          <img className="icon" src={process.env.PUBLIC_URL + '/assets/location.svg'} alt='로고' />
         </div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div className='heart'>
+          <img className="icon" src={process.env.PUBLIC_URL + '/assets/heart.svg'} alt='로고' />
+        </div>
+        <div className='home'>
+          <img className="icon" src={process.env.PUBLIC_URL + '/assets/home.svg'} alt='로고' />
+        </div>
+        <div className='search'>
+          <img className="icon" src={process.env.PUBLIC_URL + '/assets/search.svg'} alt='로고' />
+        </div>
+        <div className='friend'>
+          <img className="icon" src={process.env.PUBLIC_URL + '/assets/friend.svg'} alt='로고' />
+        </div>
+      </div>
+        
     </div>
   );
 }
