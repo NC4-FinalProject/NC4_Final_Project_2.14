140b4528cbd2b67080be29a5f2719a8792c0b752
misu <jagalji@daum.net>
2024-03-13 11:27:15 +0900

    design : redesign Header, Footer add : Sample.scss for sample page components

--- src/components/ui/layout/Header.js
+++ src/components/ui/layout/Header.js
@@ -4,20 +4,20 @@ import '../../../scss/Header.scss';
 const Header = () => {
   return (
     <div className='Header'>
-        <div className='container'>
             <div className='logo'>
                 <img src={process.env.PUBLIC_URL + '/assets/logo.svg'} alt='로고' />
             </div>
             <div className='title'>
                 검색
             </div>
-            <div className='chat'>
-                <img src={process.env.PUBLIC_URL + '/assets/icons/chat_icon.svg'} alt='채팅 아이콘'/>
+            <div className="icon-wrapper">
+                <div className='chat'>
+                    <img src={process.env.PUBLIC_URL + '/assets/icons/chat_icon.svg'} alt='채팅 아이콘'/>
+                </div>
+                <div className='alarm'>
+                    <img src={process.env.PUBLIC_URL + '/assets/icons/alarm.svg'} alt='알림 아이콘'/>
+                </div>
             </div>
-            <div className='alarm'>
-                <img src={process.env.PUBLIC_URL + '/assets/icons/alarm.svg'} alt='알림 아이콘'/>
-            </div>
-        </div>
     </div>
   );
 }
