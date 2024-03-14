45cc38edb35b7a9ec345fe2a73ab4bc3af8793bd
Robbie-Kim <robbie6673@naver.com>
2024-03-14 14:15:16 +0900

    fix : import 오류 조치

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import Selectbox from '../components/ui/Selectbox';
 import SelectboxSample from './sample/SelectboxSample';
-import Header from '../components/ui/layout/Header';
 
 const Search = () => {
   return (
--- src/pages/sample/SelectboxSample.js
+++ src/pages/sample/SelectboxSample.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import Selectbox from '../../components/ui/Selectbox';
 
-const SelectboxSample = ({options, fontSize}) => {
+
+const SelectboxSample = () => {
+  const options = ['test1', 'test2', 'test3'];
+  const fontSize = '20px';
+
   return (
     <div>
       <Selectbox options={options} fontSize={fontSize}></Selectbox>
