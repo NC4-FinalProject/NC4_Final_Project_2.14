05a67be5b65cdf3c4c74966fcfe34b95baa4f623
KimJongBeomm <whdqja0826@gmail.com>
2024-03-15 15:50:34 +0900

    Add : 모집글 리스트 컴포넌트 파일 추가 Comment : 모집글 리스트 구현중, 다음 모집글 상세 페이지 구현 예정

--- src/pages/recruitment/RecruitmentList.js
+++ src/pages/recruitment/RecruitmentList.js
@@ -1,20 +1,41 @@
 import React from 'react'
 import '../../scss/recruitment/RecruitmentList.scss';
-import ArrowBackIosNewRoundedIcon from '@mui/icons-material/ArrowBackIosNewRounded';
-import SvgButton from "../../components/ui/button/SvgButton";
-import {SvgIcon} from "@mui/material";
-import RecruitmentCommendComponent from '../../components/recruitment/RecruitmentCommendComponent.js';
+import SearchIcon from '@mui/icons-material/Search';
+import RecruitmentListContent from '../../components/recruitment/RecruitmentListContent.js';
+import RecruitmentCommendList from '../../components/recruitment/RecruitmentCommendList.js';
+import CachedIcon from '@mui/icons-material/Cached';
+import SvgButton from '../../components/ui/button/SvgButton.js';
+import { SvgIcon } from '@mui/material';
+import AddRoundedIcon from "@mui/icons-material/AddRounded";
 
 const RecruitmentList = () => {
   return (
     <div className="recruitment_container">
-        <h3>커뮤니티 추천</h3>
-        <div className="recruitment_commend">
-        <SvgButton id={'btn-move-review1'} color={'blue'} svg={<SvgIcon component={ArrowBackIosNewRoundedIcon}/>}/>
-        
-        <RecruitmentCommendComponent/>
-        <SvgButton id={'btn-move-review2'} color={'blue'} svg={<SvgIcon component={ArrowBackIosNewRoundedIcon}/>}/>
+        <div className='recruitment_title'>
+          <h3>커뮤니티 추천 </h3>
+          <CachedIcon id={'CachedIcon'}/>
         </div>
+        <div className="recruitment_commend">
+            <RecruitmentCommendList/>
+        </div>
+        <h3>모 집</h3>
+        <div className='recruitment_box'>
+              <SearchIcon id={'SearchIcon'}></SearchIcon>
+              <input className='recruitment_select' placeholder="검색"  />
+        </div>
+
+            <RecruitmentListContent/>
+            <RecruitmentListContent/>
+            <RecruitmentListContent/>
+            <RecruitmentListContent/>
+            <RecruitmentListContent/>
+            <RecruitmentListContent/>
+            <RecruitmentListContent/>
+            <RecruitmentListContent/>
+            <RecruitmentListContent/>
+            <RecruitmentListContent/>
+
+            <SvgButton id={'SvgButton'} color={'blue'} svg={<SvgIcon component={AddRoundedIcon}/>}/>
     </div>
   );
 }
