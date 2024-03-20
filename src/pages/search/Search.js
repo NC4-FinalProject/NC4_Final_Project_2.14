82cce54335f91b3488f40c07437b18ae970691bb
Robbie-Kim <robbie6673@naver.com>
2024-03-20 20:24:42 +0900

    Add : - VScode 확장 기능 사용을 위한 .cssrem 파일 추가 - Search 화면의 여행 검색 결과 컴포넌트 스타일을 위한 TravelSearchResult.scss 파일 추가,
    Feat : Search 페이지 완료

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -4,8 +4,12 @@ import SelectBox from '../../components/ui/SelectBox';
 import Input from '../../components/ui/lnput/Input';
 import Tag from '../../components/ui/Tag';
 import FriendSearchResult from './FriendSearchResult';
+import TravelSearchResult from './TravelSearchResult';
 
 const Search = () => {
+  const [selectedOption, setSelectedOption] = useState('');
+
+  const options = ['여행', '친구']
   const popularSearchList = ['검색결과', '검색결과', '검색결과', '검색결과', '검색결과', '검색결과', '검색결과', '검색결과', '검색결과', '검색결과'];
   const friendSearchResults = [
   {
@@ -37,13 +41,55 @@ const Search = () => {
     name : '김철수',
     img : 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg',
     tags : ['태그1', '태그2', '태그3']
-  }]
+  }
+  ];
+  const travelSearchResults = [
+  { img : 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg',
+    title : '여행지 제목1',
+    detailAddr : '(우편번호) 상세주소',
+    tel : '전화번호1',
+    location : '위치1',
+    like : '좋아요 개수1'
+  },
+  { 
+    img : 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg',
+    title : '여행지 제목2',
+    detailAddr : '(우편번호) 상세주소',
+    tel : '전화번호2',
+    location : '위치2',
+    like : '좋아요 개수2'
+  },
+  { 
+    img : 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg',
+    title : '여행지 제목3',
+    detailAddr : '(우편번호) 상세주소',
+    tel : '전화번호3',
+    location : '위치3',
+    like : '좋아요 개수3'
+  },
+  { 
+    img : 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg',
+    title : '여행지 제목4',
+    detailAddr : '(우편번호) 상세주소',
+    tel : '전화번호4',
+    location : '위치4',
+    like : '좋아요 개수4'
+  },
+  { 
+    img : 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg',
+    title : '여행지 제목5',
+    detailAddr : '(우편번호) 상세주소',
+    tel : '전화번호5',
+    location : '위치5',
+    like : '좋아요 개수5'
+  }
+  ];
   return (
     <div className='Search'>
       <div className='section-search-container'>
         <div className='section-search'>
           <div className='select-box-container'>
-            <SelectBox></SelectBox>
+            <SelectBox options={options}></SelectBox>
           </div>
             <div className='input-container'>
               <Input
@@ -52,17 +98,28 @@ const Search = () => {
             </div>
         </div>
         <div className='section-tags'>
-          <Tag text={'태그 1'} color={'blue'}></Tag>
-          <Tag text={'태그 2'} color={'blue'}></Tag>
-          <Tag text={'태그 3'} color={'blue'}></Tag>
-          <Tag text={'태그 4'} color={'blue'}></Tag>
-          <Tag text={'태그 5'} color={'blue'}></Tag>
+          <div className='tag-container'>
+            <Tag text={'태그 1'} color={'blue'}></Tag>
+          </div>
+          <div className='tag-container'>
+            <Tag text={'태그 2'} color={'blue'}></Tag>
+          </div>
+          <div className='tag-container'>
+            <Tag text={'태그 3'} color={'blue'}></Tag>
+          </div>
+          <div className='tag-container'>
+            <Tag text={'태그 4'} color={'blue'}></Tag>
+          </div>
+          <div className='tag-container'>
+            <Tag text={'태그 5'} color={'blue'}></Tag>
+          </div>
         </div>
       </div>
       <div className='section-search-result'>
         <div className='section-search-result-title'>
-          실시간 인기 검색어
+          검색결과
         </div>
+        {/* <TravelSearchResult travelSearchResults={travelSearchResults} ></TravelSearchResult> */}
         <FriendSearchResult friendSearchResults={friendSearchResults}></FriendSearchResult>
         {/* <div className='section-search-result-content'>
           {(popularSearchList).map((result, index) => {
@@ -73,6 +130,7 @@ const Search = () => {
               );
             })}
         </div> */}
+
       </div>
     </div>
   );
--- src/pages/search/TravelSearchResult.js
+++ src/pages/search/TravelSearchResult.js
@@ -1,8 +1,43 @@
 import React from 'react';
+import '../../scss/search/TravelSearchResult.scss';
 
-const TravelSearchResult = () => {
+const TravelSearchResult = ({ travelSearchResults }) => {
   return (
-    <div>TravelSearchResult</div>
+    <>{travelSearchResults.map((travelSearchResult, index) => (
+      <div className='TravelSearchResult'>
+        <div className='ImgContainer'>
+          <img src={travelSearchResult.img} alt='travel'></img>
+        </div>
+        <div className='ContentContainer'>
+          <div className='Content'>
+            <div className='Title'>
+              {travelSearchResult.title}
+            </div>
+            <div className='DetailAddr'>
+              {travelSearchResult.detailAddr}
+            </div>
+            <div className='Tel'>
+              {travelSearchResult.tel}
+            </div>
+            <div className='LocationAndLikeContainer'>
+              <div className='LocationContainer'>
+                <div className='LocationIcon'></div>
+                <div className='Location'>
+                  {travelSearchResult.location}
+                </div>
+              </div>
+              <div className='LikeContainer'>
+                <div className='LikeIcon'></div>
+                <div className='Like'>
+                  {travelSearchResult.like}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    ))}
+    </>
   );
 }
 
