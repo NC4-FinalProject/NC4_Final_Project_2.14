6703ef5b78cd7835cf16dabb03582af76a6a5a74
Robbie-Kim <robbie6673@naver.com>
2024-03-21 17:56:21 +0900

    Feat : 채팅 페이지 채팅 목록 list화 구현 완료

--- src/pages/chat/Chat.js
+++ src/pages/chat/Chat.js
@@ -1,7 +1,45 @@
 import React from 'react';
 import '../../scss/pages/chat/Chat.scss';
+import HoverDescription from '../../components/ui/HoverDescription';
 
 const Chat = () => {
+  // 예시 매개변수 리스트 : 각 채팅방 고유 번호, 각 채팅 상대의 사진, 각 채팅의 마지막 메세지, 각 채팅의 안읽은 메세지 수, 각 채팅 상대의 이름
+  const chatRoomList = [
+    {
+      chatRoomNo: 1,
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7MnOcQUfqtgTKRpCld7E-_P2JCyF-QMlesD887gUZ6A&s',
+      lastChat: '안녕하세요',
+      unreadCnt: 1,
+      name: '김태현1'
+    },
+    {
+      chatRoomNo: 2,
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7MnOcQUfqtgTKRpCld7E-_P2JCyF-QMlesD887gUZ6A&s',
+      lastChat: '안녕히가세요',
+      unreadCnt: 2,
+      name: '김태현2'
+    },
+    {
+      chatRoomNo: 3,
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7MnOcQUfqtgTKRpCld7E-_P2JCyF-QMlesD887gUZ6A&s',
+      lastChat: '화이팅이에요',
+      unreadCnt: 51,
+      name: '김태현3'
+    },
+    {
+      chatRoomNo: 4,
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7MnOcQUfqtgTKRpCld7E-_P2JCyF-QMlesD887gUZ6A&s',
+      lastChat: '화이팅이에요',
+      unreadCnt: 4,
+      name: '김태현4'
+    }
+  ]
+
+  
+
+
+  const hoverDescription = '삭제';
+
   const requestCnt = 0;
   const chatListCnt = 0;
 
@@ -16,12 +54,12 @@ const Chat = () => {
           <h2>친구 요청 ({requestCnt})</h2>
         </div>
         <div className='chat-request-list'>
-          <div className='friend-container'>
-            <div className='friend-img-container'>
-              <img className='friend-img' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7MnOcQUfqtgTKRpCld7E-_P2JCyF-QMlesD887gUZ6A&s'></img>
-              <div className='cancle-img' onClick={handleFriendDelete}></div>
+          <div className='request-friend-container'>
+            <div className='request-friend-img-container'>
+              <img className='request-friend-img' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7MnOcQUfqtgTKRpCld7E-_P2JCyF-QMlesD887gUZ6A&s'></img>
+              <div id='delete' className='request-cancle-img' onClick={handleFriendDelete}></div>
             </div>
-            <span className='friend-name'>
+            <span className='request-friend-name'>
               김태현
             </span>
           </div>
@@ -34,25 +72,28 @@ const Chat = () => {
         채팅목록 ({chatListCnt})
       </h2>
       <div className='chat-list-container'>
-        <div className='chat-list'>
-          <div className='friend-img-container'>
-            <div className='friend-name'>
-              <p>이름</p>
-            </div>
-            <img className='friend-img' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7MnOcQUfqtgTKRpCld7E-_P2JCyF-QMlesD887gUZ6A&s'>
-            </img>
-          </div>
-          <div className='last-chat'>
-
-          </div>
-          <div className='chat-cnt-icon-container'>
-            <div className='icon'>
-              <div className='cnt'>
-                
+        {chatRoomList.map((chatRoom, index) => {
+          return (
+            <div className='chat-list' key={index}>
+              <div className='friend-container'>
+                <div className='friend-name'>
+                  <p>{chatRoom.name}</p>
+                </div>
+                  <div className='friend-img-container'>
+                    <img className='friend-img' src={chatRoom.img}></img>
+                  </div>
               </div>
-            </div>
-          </div>
-        </div>
+              <div className='last-chat'>
+                <p>{chatRoom.lastChat}</p>
+              </div>
+              <div className='chat-cnt-icon-container'>
+                <div className='chat-cnt-icon'>
+                  <p className='chat-cnt'>{chatRoom.unreadCnt}</p>
+                </div>
+              </div>
+            </div> 
+          )
+        })}
       </div>
     </div>
   );
