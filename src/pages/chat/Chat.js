3c46581c332a79374727bf83dc0a3c08685c2135
Robbie-Kim <robbie6673@naver.com>
2024-03-21 14:06:00 +0900

    Add : 채팅 페이지 삭제 버튼 표현을 위한 svg 파일 추가, Feat : 채팅 페이지 친구 요청 칸 구현, Design : 채팅 페이지 구현을 위한 스타일 작업

--- src/pages/chat/Chat.js
+++ src/pages/chat/Chat.js
@@ -3,24 +3,55 @@ import '../../scss/pages/chat/Chat.scss';
 
 const Chat = () => {
   const requestCnt = 0;
+  const chatListCnt = 0;
+
+  const handleFriendDelete = () => {
+    console.log('handleFriendDelete');
+  }
 
   return (
     <div className='Chat'>
       <div className='chat-request-container'>
         <div className='chat-request-title'>
-          <h2 className='title'>친구 요청 ({requestCnt})</h2>
+          <h2>친구 요청 ({requestCnt})</h2>
+        </div>
+        <div className='chat-request-list'>
+          <div className='friend-container'>
+            <div className='friend-img-container'>
+              <img className='friend-img' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7MnOcQUfqtgTKRpCld7E-_P2JCyF-QMlesD887gUZ6A&s'></img>
+              <div className='cancle-img' onClick={handleFriendDelete}></div>
+            </div>
+            <span className='friend-name'>
+              김태현
+            </span>
+          </div>
         </div>
       </div>
 
-      <div className='chat-request-list'>
-        <div className='friend-container'>
+
+
+      <h2 className='chat-list-title'>
+        채팅목록 ({chatListCnt})
+      </h2>
+      <div className='chat-list-container'>
+        <div className='chat-list'>
           <div className='friend-img-container'>
-            <img className='friend-img' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7MnOcQUfqtgTKRpCld7E-_P2JCyF-QMlesD887gUZ6A&s'></img>
-            <img className='cancle-img'></img>
+            <div className='friend-name'>
+              <p>이름</p>
+            </div>
+            <img className='friend-img' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7MnOcQUfqtgTKRpCld7E-_P2JCyF-QMlesD887gUZ6A&s'>
+            </img>
           </div>
-          <span className='friend-name'>
-            김태현
-          </span>
+          <div className='last-chat'>
+
+          </div>
+          <div className='chat-cnt-icon-container'>
+            <div className='icon'>
+              <div className='cnt'>
+                
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
