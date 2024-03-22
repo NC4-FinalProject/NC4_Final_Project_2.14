a2acb5aec7a1e88786a577e6a7df33e33e9a9a97
misu <jagalji@daum.net>
2024-03-22 17:32:42 +0900

    Feat : add review to TravelInfo page Design : edit MyReview, ReviewList, MyCommunity, TravelInfo

--- src/pages/travel/ViewTravelInfo.js
+++ src/pages/travel/ViewTravelInfo.js
@@ -1,6 +1,8 @@
+import '../../scss/review/ReviewList.scss';
 import '../../scss/pages/travel/TravelInfo.scss';
 import TravelInfo from "../../components/travel/TravelInfo";
 import TravelDetailInfo from "../../components/travel/TravelDetailInfo";
+import ReviewListContentList from "../../components/review/ReviewListContentList";
 
 const ViewTravelInfo = () => {
     const contentType = 12;
@@ -8,6 +10,9 @@ const ViewTravelInfo = () => {
         <div className="ViewTravelInfo">
             <TravelInfo contentType={contentType}>
                 <TravelDetailInfo contentType={contentType}/>
+                <div className='reviewList_container'>
+                    <ReviewListContentList/>
+                </div>
             </TravelInfo>
         </div>
     );
