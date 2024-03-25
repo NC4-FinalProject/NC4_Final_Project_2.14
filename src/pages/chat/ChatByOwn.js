c770810243f036055baa9ed2c9db556bb6742624
Robbie-Kim <robbie6673@naver.com>
2024-03-25 10:51:14 +0900

    Add : - Chat 기능 구현을 위한 Stomp 의존성 추가 - 채팅방 기능 구현을 위한 상대 채팅 컴포넌트 추가 - 자신 채팅 컴포넌트 추가

---
+++ src/pages/chat/ChatByOwn.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ChatByOwn = () => {
+  return (
+    <div>ChatByOwn</div>
+  );
+}
+
+export default ChatByOwn;
---
+++ src/pages/chat/ChatByPartner.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ChatByPartner = () => {
+  return (
+    <div>ChatByPartner</div>
+  );
+}
+
+export default ChatByPartner;
