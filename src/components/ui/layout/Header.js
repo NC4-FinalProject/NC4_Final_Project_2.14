9ef1a77ddf9ed67d86fe9fc5217d8ba4fac7ce51
misu <jagalji@daum.net>
2024-04-17 12:08:09 +0900

    Refactor

--- src/components/ui/layout/Header.js
+++ src/components/ui/layout/Header.js
@@ -1,52 +1,50 @@
-import React, { useEffect, useState, useRef, useMemo } from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import '../../../scss/Header.scss';
-import { useLocation, useNavigate } from 'react-router-dom';
+import {useLocation, useNavigate} from 'react-router-dom';
 import ToggleMenu from '../ToggleMenu';
-import { useSelector } from 'react-redux';
+import {useSelector} from 'react-redux';
 
 const Header = () => {
     const navi = useNavigate();
     const location = useLocation();
     const isHomePage = location.pathname === '/';
-    const logoColor = isHomePage ? 'white' : 'black';
-    
-    // 로그인 여부 확인
+    const logoColor = isHomePage ? 'white' : 'color';
+    const iconColor = isHomePage ? 'white' : 'black';
+
     const isLogin = useSelector(state => state.userSlice.isLogin);
 
     const [menuOpen, setMenuOpen] = useState(false);
     const anchorRef = useRef(null);
     const toggleMenu = () => {
-      setMenuOpen((prevOpen) => !prevOpen);
+        setMenuOpen((prevOpen) => !prevOpen);
     };
 
     const items = [
-        { label: 'Item 1', onClick: () => console.log('Item 1 selected') },
-        { label: 'Item 2', onClick: () => console.log('Item 2 selected') },
-        { label: 'Item 3', onClick: () => console.log('Item 3 selected') },
-        { label: 'Item 4', onClick: () => console.log('Item 4 selected') },
-        { label: 'Item 5', onClick: () => console.log('Item 5 selected') },
-        { label: 'Item 6', onClick: () => console.log('Item 6 selected') },
-        { label: 'Item 7', onClick: () => console.log('Item 7 selected') } 
+        {label: 'Item 1', onClick: () => console.log('Item 1 selected')},
+        {label: 'Item 2', onClick: () => console.log('Item 2 selected')},
+        {label: 'Item 3', onClick: () => console.log('Item 3 selected')},
+        {label: 'Item 4', onClick: () => console.log('Item 4 selected')},
+        {label: 'Item 5', onClick: () => console.log('Item 5 selected')},
+        {label: 'Item 6', onClick: () => console.log('Item 6 selected')},
+        {label: 'Item 7', onClick: () => console.log('Item 7 selected')}
     ];
 
-
-    // 현재 경로에 따라 타이틀 반환
     const useTitle = () => {
-        const pathName = location.pathname.split('/')[1];
+        const pathName = location.pathname;
 
         switch (pathName) {
-            case '':
-                return '';
-            case 'chat':
+            case '/chat':
                 return '채팅';
-            case 'alarm':
+            case '/alarm':
                 return '알림';
-            case 'search':
+            case '/search':
                 return '검색';
-            case 'mypage':
+            case '/mypage':
                 return '마이페이지';
-            case 'user':
+            case '/user/sign-in':
                 return '로그인';
+            case '/user/sign-up':
+                return '회원가입';
             default:
                 return '';
         }
@@ -57,11 +55,10 @@ const Header = () => {
         // setAlarm(true);
     }, []);
 
-
     return (
         <div className='Header'>
             <div className='logo'>
-                <img src={process.env.PUBLIC_URL + `/assets/logo_${logoColor}.svg`} alt='로고'/>
+                <img src={process.env.PUBLIC_URL + `/logo_${logoColor}.png`} alt='로고'/>
             </div>
             <div className='title'>
                 {useTitle()}
@@ -70,20 +67,19 @@ const Header = () => {
                 {isLogin ? (
                     <>
                         <div className='chat' onClick={() => navi('/chat')}>
-                            <img className='img' src={process.env.PUBLIC_URL + `/assets/icons/chat_${logoColor}.svg`} alt='채팅 아이콘'/>
+                            <img className='img' src={process.env.PUBLIC_URL + `/assets/icons/chat_${iconColor}.svg`}
+                                 alt='채팅 아이콘'/>
                         </div>
-                        <div
-                            className='alarm'
-                            ref={anchorRef}
-                        >
-                            <img className='img' src={process.env.PUBLIC_URL + `/assets/icons/alarm_${logoColor}.svg`} alt='알림 아이콘' 
-                            onClick={toggleMenu}
-                            />
+                        <div className='alarm' ref={anchorRef}>
+                            <img className='img' src={process.env.PUBLIC_URL + `/assets/icons/alarm_${iconColor}.svg`}
+                                 alt='알림 아이콘'
+                                 onClick={toggleMenu}/>
                         </div>
                     </>
                 ) : (
                     <div className='login' onClick={() => navi('/user/sign-in')}>
-                        <img className='img' src={process.env.PUBLIC_URL + `/assets/icons/login_${logoColor}.svg`} alt='로그인 아이콘'></img>    
+                        <img className='img' src={process.env.PUBLIC_URL + `/assets/icons/login_${iconColor}.svg`}
+                             alt='로그인 아이콘'/>
                     </div>
                 )}
                 <ToggleMenu
@@ -91,28 +87,7 @@ const Header = () => {
                     anchorEl={anchorRef.current}
                     open={menuOpen}
                     onClose={() => setMenuOpen(false)}
-                >
-                </ToggleMenu>
-
-                {/* 원본 */}
-                {/* <div className='chat' onClick={() => navi('/chat')}>
-                    <img src={process.env.PUBLIC_URL + `/assets/icons/chat_${logoColor}.svg`} alt='채팅 아이콘'/>
-                </div>
-                <div
-                    className='alarm'
-                    ref={anchorRef}
-                >
-                    <img src={process.env.PUBLIC_URL + `/assets/icons/alarm_${logoColor}.svg`} alt='알림 아이콘' 
-                     onClick={toggleMenu}
-                     />
-                    <ToggleMenu
-                        items={items}
-                        anchorEl={anchorRef.current}
-                        open={menuOpen}
-                        onClose={() => setMenuOpen(false)}
-                    >
-                    </ToggleMenu>
-                </div> */}
+                />
             </div>
         </div>
     );
--- src/pages/sign/SignIn.js
+++ src/pages/sign/SignIn.js
@@ -1,73 +1,25 @@
 import React, {useCallback, useState} from 'react';
 import Input from '../../components/ui/lnput/Input.js';
-import { useForm } from 'react-hook-form';
 import '../../scss/pages/sign/Sign.scss';
 import FullWidthButton from '../../components/ui/button/FullWidthButton.js';
-import { signin } from '../../apis/userApi.js';
-import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import {signin} from '../../apis/userApi.js';
+import {useDispatch} from 'react-redux';
+import {useNavigate} from 'react-router-dom';
 
 function SignIn() {
-    // const {
-    //   register,
-    //   formState: { errors },
-    //   handleSubmit,
-    //   watch,
-    // } = useForm({ mode: 'onChange' });
-
     const navi = useNavigate();
 
-    const [autoLogin, setAutoLogin] = useState(false);
-    // const [idError, setIdError] = useState('');
-    // const [passwordError, setPasswordError] = useState('');
-
-    // const id = watch('id');
-    // const password = watch('password');
     const dispatch = useDispatch();
 
-    // const handleSignIn = (data) => {
-      // if (Object.keys(errors).length !== 0) {
-      //   console.error("폼 데이터에 유효성 검사 에러가 있습니다.");
-      //   return;
-      // }
-
-      // const loginData = {
-      //   id: data.id,
-      //   pw: data.password,
-      // };
-
-      // dispatch(signin(loginData))
-      //   .then((response) => {
-      //     console.log(response);
-      //     const user = response.payload.item;
-      //     console.log(user.token);
-      //     navi("/");
-      //   })
-      //   .catch((error) => {
-      //     console.error('Login failed:', error.payload);
-      //     const errorMessage =
-      //       error.payload.error === 'invalid_id' || error.payload.error === 'invalid_password'
-      //         ? '아이디 또는 비밀번호가 틀렸습니다. 다시 입력해주세요.'
-      //         : '알 수 없는 에러가 발생했습니다.';
-      //     setIdError(errorMessage);
-      //     setPasswordError(errorMessage);
-      //   });
-    // };
-
-    // const handleAutoLoginChange = (e) => {
-    //   setAutoLogin(e.target.checked);
-    // };
-
-    // 여기서부터 새로운 로직입니다.
     const [form, setForm] = useState({
-       userId : '',
-       userPw : ''
+        userId: '',
+        userPw: ''
     });
 
     const inputFieldChanged = useCallback((e) => {
         setForm((prevForm) => ({
             ...prevForm,
-            [e.target.name] : e.target.value,
+            [e.target.name]: e.target.value,
         }));
     }, []);
 
@@ -79,79 +31,44 @@ function SignIn() {
     }, [form, dispatch, navi]);
 
     return (
-      <div className="SignIn">
-          {/*<form id="form-sign-in" onSubmit={handleSubmit(handleSignIn)}>*/}
-          <form id="form-sign-in" onSubmit={rebuildHandleSignIn}>
-                  <img className="traveler" src={process.env.PUBLIC_URL + "/assets/icons/login_background.png"}
-                       alt="Traveler"/>
-                  <h1 className="main-logo">logo</h1>
-                  <div className='input-container'>
-                      <p className="id">아이디</p>
-                      {/*<Input
-                          type="id"
-                          name="id"
-                          placeholder="아이디를 입력해주세요."
-
-                          {...register('id', {
-                              required: '아이디를 입력해주세요.',
-                              onChange: () => setIdError(''),
-                          })}
-                      />*/}
-                      <Input
-                          type="text"
-                          name="userId"
-                          placeholder="아이디를 입력해주세요."
-                          value={form.userId}
-                          onChange={inputFieldChanged}
-                      />
-                      {/*{!id && errors.id && <span className="error-message">{errors.id.message}</span>}*/}
-                      <div className='input-container'>
-                      </div>
-                      <p className="text-color">비밀번호</p>
-                      {/*<Input
-                          type="password"
-                          name="password"
-                          placeholder="비밀번호를 입력해주세요."
-                          {...register('password', {
-                              required: '비밀번호를 입력해주세요.',
-                              onChange: () => setPasswordError(''),
-                          })}
-                      />*/}
-                      <Input
-                          type="password"
-                          name="userPw"
-                          placeholder="비밀번호를 입력해주세요."
-                          value={form.userPw}
-                          onChange={inputFieldChanged}
-                      />
-                      {/*{!password && errors.password && <span className="error-message">{errors.password.message}</span>}*/}
-                      {/*{passwordError && <span className="error-message">{passwordError}</span>}*/}
-                  </div>
-                  <div className="login-options">
-                      <div className="auto-login">
-                          <input
-                              type="checkbox"
-                              id="autoLogin"
-                              checked={autoLogin}
-                              // onChange={handleAutoLoginChange}
-                          />
-                          <label htmlFor="autoLogin">자동 로그인</label>
-                      </div>
-                      <div className="find-account">
-                          <a href="/user/sign-up">회원가입 | </a><a href="/">아이디/비밀번호 찾기</a>
-                      </div>
-                  </div>
-                  <FullWidthButton color="green" text="로그인" type="submit"/>
-                  <hr className="hr1"/>
-                  <p className="p3">간편 로그인</p>
-                  <hr className="hr2"/>
-                  <img className="kakao-login" src={process.env.PUBLIC_URL + "/assets/icons/kakao_login.png"}
-                       alt="Kakao-login"/>
-                  <img className="google-login" src={process.env.PUBLIC_URL + '/assets/icons/google_login.png'}
-                       alt="Google-login"/>
-              </form>
-      </div>
-);
+        <div className="SignIn">
+            <form id="form-sign-in" onSubmit={rebuildHandleSignIn}>
+                <img
+                    src={process.env.PUBLIC_URL + "/assets/boba-boys-going-to-school-together.png"}
+                    alt=""/>
+                <h1 className="desc">Let's travel together</h1>
+                <div className='input-container'>
+                    <p>아이디</p>
+                    <Input
+                        type="text"
+                        name="userId"
+                        placeholder="아이디를 입력해주세요."
+                        value={form.userId}
+                        onChange={inputFieldChanged}/>
+                    <p>비밀번호</p>
+                    <Input
+                        type="password"
+                        name="userPw"
+                        placeholder="비밀번호를 입력해주세요."
+                        value={form.userPw}
+                        onChange={inputFieldChanged}/>
+                    <div className="find-account">
+                        <a href="/user/sign-up">회원가입</a>&nbsp;|&nbsp;<a href="/">아이디/비밀번호 찾기</a>
+                    </div>
+                </div>
+                <FullWidthButton color="green" text="로그인" type="submit"/>
+                <div className="social-login">
+                    <span>간편 로그인</span>
+                </div>
+                <div className="banner-wrapper">
+                    <img className="kakao-login" src={process.env.PUBLIC_URL + "/assets/icons/kakao_login.png"}
+                         alt="Kakao-login"/>
+                    <img className="google-login" src={process.env.PUBLIC_URL + '/assets/icons/google_login.png'}
+                         alt="Google-login"/>
+                </div>
+            </form>
+        </div>
+    );
 }
 
 export default SignIn
